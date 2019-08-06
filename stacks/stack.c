1ad402ee02ceea5dd5b5aed5d89fecc0daefc125
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-08-06 13:01:03 +0200

    define the pop() for stacks

--- stacks/stack.c
+++ stacks/stack.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//prototypes
-int push(struct node** top, int value);
 
-struct node{
+
+
+struct node {
 	int data;
 	struct node * pNext;
 };
 
+//prototypes
+int push(struct node** top, int value);
+void printStack(struct node* top);
+int pop(struct node** top);
 
 int main(void){
-
+	struct node * pTop = NULL;
+	push(&pTop, 3);
+	push(&pTop, 8);
+	push(&pTop, 11);
+	pop(&pTop);
+	pop(&pTop);
+	printStack(pTop);
 	return 0;
 }
 
 int push(struct node** top, int value){
 	int retval = 0;
-	struct node* ptrNew = (struct node*)malloc(sizeof(struct node));
+	struct node* ptrNew = (struct node*) malloc(sizeof(struct node));
 	if(ptrNew){ // if allocated successfully
 		ptrNew->data = value;
 		ptrNew->pNext = *top; //whatever the top is looking at
 		*top = ptrNew;
 		retval = 1;
 	} else {
-		printf("%d not pushed, memory allocation failed!", value);
+		printf("%d not pushed, memory allocation failed!\n", value);
 	}
 	return retval;
 }
@@ -36,14 +46,21 @@ void printStack(struct node* top){
 	} else {
 		puts("The stack is: ");
 		while(top != NULL){
-			printf("%d -->", top->data);
+			printf(" %d -->", top->data);
 			top = top->pNext;
 		}	
-		printf("NULL\n");
+		printf(" NULL\n");
 	}
 	
 }
 
 
-
+int pop(struct node** top){
+	retval = -1;
+	struct node * pTemp = *top;
+	*top = pTemp->pNext;
+	retval = pTemp->data;
+	free(pTemp);
+	return retval;
+}
 
