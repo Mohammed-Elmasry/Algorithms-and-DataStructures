05569bd6c2722688223fa706a8b1ae10451781b3
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-08-09 00:58:00 +0200

    define the append() for the double linked list

--- linked_lists/double_linked_list.c
+++ linked_lists/double_linked_list.c
@@ -10,8 +10,36 @@ struct node {
 };
 
 //prototypes
+int append(struct node ** head, struct node** tail, char value);
 
 int main(void){
-	
+	struct node * pHead = NULL;
+	struct node * pTail = NULL;
+
+	append(&pHead, &pTail, 'C');
 	return 0;
 }
+
+
+int append(struct node ** head, struct node** tail, char value){
+	int retval = 0;
+	struct node* ptrNew = (struct node*) malloc(sizeof(struct node));
+	if(ptrNew != NULL){
+		ptrNew->data = value;
+		ptrNew->pNext = NULL;
+		ptrNew->pPrev = NULL;
+
+		if(*head == NULL){ // empty list
+			*head = ptrNew;
+		} else { // list is not empty
+			(*tail)->pNext = ptrNew;
+			ptrNew->pPrev = (*tail);
+		}
+		*tail = ptrNew;
+		retval = 1;
+	} else {
+		printf("%c not inserted, memory allocation failed!", value);
+		retval = 0;
+	}
+}
+
