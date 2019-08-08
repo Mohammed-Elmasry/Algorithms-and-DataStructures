01d6805a131201ce7999b0c5b85d9ee62250e3b8
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-08-08 12:50:05 +0200

    define the printQueue() function and the isEmpty() check function

--- queues/queues.c
+++ queues/queues.c
@@ -7,10 +7,13 @@ struct node {
 };
 
 //prototypes
+void printQueue(struct node * head);
+int enqueue(struct node** head, struct node** tail, char value);
+int isEmpty(struct node * ptr);
 
 int main(void){
 	struct node *pHead, *pTail = NULL;
-
+	enqueue(&pHead, &pTail, 'C');
 	return 0;
 }
 
@@ -20,11 +23,33 @@ int enqueue(struct node** head, struct node** tail, char value){
 	struct node * ptrNew = (struct node *)malloc(sizeof(struct node));
 	if (ptrNew){
 		ptrNew->data = value;
-		ptrNew->pNext = (*tail)->pNext;
-		(*tail)->pNext = ptrNew;
-		*tail = ptrNew;
+		ptrNew->pNext = NULL;
+
+		if(isEmpty(*head)){
+			(*head)->pNext = ptrNew;
+		} else {
+			(*tail)->pNext = ptrNew;	
+		}
+		*tail = ptrNew; //the tail has to point to the newly added node anyway
+		retval = 1;
 	} else {
 		printf("%c not inserted, memory allocation failed!", value);
 	}
 	return retval;
+}
+
+void printQueue(struct node * head){
+	if (head == NULL){
+		puts("Queue is empty!\n");
+	}
+
+	while(head != NULL){
+		printf("%c --> ", head->data);
+		head = head->pNext;
+	}
+	puts(" NULL\n");
+}
+
+int isEmpty(struct node * ptr){
+	return ptr == NULL;
 }
