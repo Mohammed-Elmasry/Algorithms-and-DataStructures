4d6a6e6c22c259cb96ad9db25dfe7cdef2197e97
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-08-16 18:46:50 +0200

    define the expandByOne(array, size, element) to append the new element to the end of a flexible arrayList (not working)

--- arrayLists/arrayList.c
+++ arrayLists/arrayList.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct arrayList{
-	int array[4];
+	int * array;
 }typedef ArrayList;
 
 // prototypes
 void printList(ArrayList list, long unsigned int size);
-void expandByOne();
+void expandByOne(ArrayList *list, long unsigned int size, int element);
 
 
 int main(void){
 	ArrayList list; 
-	list.array[0] = 10;
-	list.array[1] = 11;
-	list.array[2] = 12;
-	list.array[3] = 13;
+	int array[5] = {1,3,5,2,6};
+	list.array = array;
 
-	printList(list, 4);
+	// expandByOne(&list, 4);
+	expandByOne(&list, 5, 19);
+	
+	long unsigned int size = sizeof(list.array);
+	printf("size of arrayList is %lu\n", size);
+	printList(list, 5);
 	return 0;
 }
 
-void expandByOne(){
+void expandByOne(ArrayList *list, long unsigned int size, int element){
+	int * ptr = (int *) malloc(sizeof(int) * (size + 1));
+	memcpy(ptr, list->array, size);
 
-
+	ptr[size+1] = element;
+	list->array = ptr;
 }
 
 void printList(ArrayList list, long unsigned int size){
