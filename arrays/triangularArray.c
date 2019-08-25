47413d98a0692f3679f95c527004dacd6d107cd2
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-08-25 14:53:09 +0200

    define a double Stack structure with its push() along with validation

---
+++ arrays/triangularArray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//prototypes
+
+int main(void){
+	int * x[2];
+
+
+	return 0;
+}
+
+
+
+int allocate(unsigned int index){
+	
+	for (i = 0; i < index; ++i){
+
+	}
+}
---
+++ stacks/doubleStack.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct DoubleStack {
+	char arr[10];  // 0 to 4 , 5 to 9
+	int topLeft;
+	int topRight;
+	int size;
+};
+
+// prototypes
+int push (struct DoubleStack * stack, char c, char * flag);
+
+
+int main(void){
+
+	//initialization for the stack
+	struct DoubleStack stack = {{},0,9,10};
+	push(&stack, 'A', "left");
+	push(&stack, 'P', "left");
+	push(&stack, 'P', "left");
+	push(&stack, 'L', "left");
+	push(&stack, 'E', "left");
+	push(&stack, 'A', "left");
+
+	printf("%c\n", stack.arr[0]);
+	return 0;
+}
+
+
+
+int push (struct DoubleStack * stack, char c, char * flag){
+	int retval = 0;
+	if(flag == "left"){
+		if(stack->topLeft < (stack->size) / 2){
+			stack->arr[stack->topLeft] = c;
+			stack->topLeft++;
+			retval = 1;	
+		} else {
+			puts("top of stack reached!");
+			retval = 0;
+		}		
+	} else {
+		if(stack->topRight >= (stack->size) / 2){
+			stack->arr[stack->topRight] = c;
+			stack->topRight--;
+			retval = 1;	
+		} else {
+			puts("top of stack reached!");
+			retval = 0;
+		}
+	}
+	return retval;
+}
