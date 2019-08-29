8f3698210855b0df5657fea01a29de55991d4e64
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-08-29 11:45:45 +0200

    define the swapLeft()

--- queues/arrayQueue.c
+++ queues/arrayQueue.c
@@ -8,24 +8,36 @@ struct Queue {
 
 //prototypes
 int dequeue(struct Queue * que);
-int enqueue(struct Queue * que, int element);
+void swapLeft(struct Queue * que, unsigned int size);
 void printQueue(struct Queue que, unsigned int size);
 void initializeQueue(struct Queue * que, unsigned int size);
+int enqueue(struct Queue * que, int element, unsigned int size);
 
 
-int main(int argc, char argv[]){
+int main(int argc, char * argv[]){
 	struct Queue q;
 	int length = sizeof(q.arr) / sizeof(int);
 	initializeQueue(&q, length);
+
+	enqueue(&q, 5, length);
+	enqueue(&q, 19, length);
+	enqueue(&q, 8, length);
+	enqueue(&q, 3, length);
+	enqueue(&q, 2, length);
+	enqueue(&q, 1, length);
+	enqueue(&q, 0, length);
+
+	printQueue(q, length);
 	return 0;
 }
 
-int enqueue(struct Queue * que, int element){
+int enqueue(struct Queue * que, int element, unsigned int size){
 	int retval = 0;
 	//first check if enqueue is possible...
 	if(que->head > 0){
-		que->arr[que->head] = element;
+		swapLeft(que, size);
 		que->head--;
+		que->arr[size - 1] = element;
 	} else {
 		puts("enqueue failed, no space to insert item!");
 	}
@@ -33,16 +45,25 @@ int enqueue(struct Queue * que, int element){
 }
 
 int dequeue(struct Queue * que){
-
+	puts("body goes in here...");
+	return 1;
 }
 
 void initializeQueue(struct Queue * que, unsigned int size){
-	que->head = size - 1;
+	que->head = size;
 }
 
 void printQueue(struct Queue que, unsigned int size){
-	for (int i = que.head; i < size - 1; ++i){
-		printf("%d ", que.arr[i]);
+	for (int i = que.head; i < size; ++i){
+		printf("%d \n", que.arr[i]);
 	}
 	puts("display complete...");
+}
+
+void swapLeft(struct Queue * que, unsigned int size){
+	if(que->head > 0){
+		for(int i = que->head; i < size; i++){
+			que->arr[i-1] = que->arr[i];
+		}
+	}
 }
