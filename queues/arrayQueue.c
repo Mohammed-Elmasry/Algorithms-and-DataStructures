78c1258d630949726b4bd1afbc6c69c119faac8a
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-08-29 09:26:28 +0200

    define the basic framework for enqueue() and dequeue() for arrayQueues

---
+++ queues/arrayQueue.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct queue {
+	int arr[6] = {};
+
+};
+
+//prototypes
+int dequeue(struct queue * que);
+int enqueue(struct queue * que, int element);
+
+
+int main(int argc, char argv[]){
+
+	return 0;
+}
+
+int enqueue(struct queue * que, int element){
+
+}
+
+int dequeue(struct queue * que){
+
+}
