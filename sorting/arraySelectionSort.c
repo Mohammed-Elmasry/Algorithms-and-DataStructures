1eb19815b42b4372f7552ecf231ed9288d7873ed
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-02 17:13:24 +0200

    clean the code in arraySelectionSort.c

--- sorting/arraySelectionSort.c
+++ sorting/arraySelectionSort.c
@@ -24,7 +24,7 @@ void selectionSort(int * array, unsigned int size){
 	for (int i = 0; i < size; ++i){
 		min = i;
 		for(int j = i + 1; j < size; ++j){
-			if(array[j] < array[min]){
+			if(array[j] < array[main]){
 				min = j;
 			}
 		}
