cc273cc7fc8871c017c47b83d40309c011079c8b
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-02 11:25:15 +0200

    change the parameter list of swapIndex() to leave out the unused size variable

--- sorting/arraySelectionSort.c
+++ sorting/arraySelectionSort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 4;
+#define SIZE 4
 
 
 //prototypes
@@ -11,21 +11,28 @@ void swapIndex(int * array, unsigned int first, unsigned int second);
 
 int main(int argc, char * argv[]){
 	int arr[SIZE] = {1,2,3,4};
-	swap(arr, 0, 3);
+	puts("before swap");
+	printArray(arr, SIZE);
+	swapIndex(arr, 0, 3);
+	puts("after swap");
+	printArray(arr, SIZE);
 	return 0;
 }
 
 void selectionSort(int * array, unsigned int size){
 	int limit = 0;
 	int max = 0;
+	int last = 0;
 	for (int i = 0; i < size - limit; i++){
 		max = array[i];
 		for(int j = i + 1; j < size - limit; j++){
 			if(array[max] < array[j]){
 				max = j;
+				last = j;
 			}
 		}
-		swap(array, size, max, j);
+		swapIndex(array, max, last);
+		limit++;
 	}
 }
 
