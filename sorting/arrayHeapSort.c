6c9115b75ae5bbe1f7e1e654f2115900d7084ba9
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-05 19:07:40 +0200

    define swapIndex() within arrayHeapSort.c to swap indices of an array

--- sorting/arrayHeapSort.c
+++ sorting/arrayHeapSort.c
@@ -4,8 +4,8 @@
 int parent(int index);
 
 int main(int argc, char * argv[]){
-	int arr[7] = {15,22,3,18,4,8,77,9};
-	printf("parent of 7 is %d\n", parent(7));
+	int arr[8] = {15,22,3,18,4,8,77,9};
+
 	return 0;
 }
 
@@ -20,5 +20,11 @@ void heapify(int * array, int size){
 }
 
 int parent(int index){
-	return (i-1)/2;
+	return (index-1)/2;
+}
+
+void swapIndex(int * array, unsigned int first, unsigned int second){
+	int temp = array[second];
+	array[second] = array[first];
+	array[first] = temp;
 }
