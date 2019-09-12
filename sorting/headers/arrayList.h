fc479462fb1c245416c01567c71ad36a9ddb7cb4
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-12 16:34:55 +0200

    define headers of arrayList structure within the headers directory

---
+++ sorting/headers/arrayList.h
@@ -0,0 +1,3 @@
+int length(int array[], long unsigned int size);
+int * expandByOne(int array[], long unsigned int size, int element);
+int * reduceByOne(int array[], long unsigned int size, int index);
