eb28df0e193f7d0144461d767e0142d6300aced2
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-19 18:36:11 +0200

    rectify the logic for array subdivision in binarySearch()

--- searching/binarySearch.c
+++ searching/binarySearch.c
@@ -2,12 +2,13 @@
 
 //prototypes
 unsigned int binarySearch(int * array, int value, int min, int max);
+unsigned int binarySearchRecursive(int * array, int value, int min, int max);
 
 
 int main(int argc, char * argv[]){
 	int arr[11] = {0,23,45,66,77,81,98,102,137,201,276};
-	
-	int result = binarySearch(arr, 81, 0, sizeof(arr) / sizeof(int));
+	int size = sizeof(arr) / sizeof(int);
+	int result = binarySearch(arr, 137, 0, size - 1);
 	
 	printf("result is %d\n", result);
 	return 0;
@@ -20,9 +21,24 @@ unsigned int binarySearch(int * array, int value, int min, int max){
 	while (min <= max){
 		mid = (min + max) / 2;
 		if(array[mid] < value){
-			max = mid - 1;
-		} else if (array[mid] > value){
 			min = mid + 1;
+		} else if (array[mid] > value){
+			max = mid - 1;
+		} else {
+			return mid;
+		}
+	}
+	return retval;
+}
+
+unsigned int binarySearchRecursive(int * array, int value, int min, int max){
+	int retval = -1;
+	int mid = (min + max) / 2;
+	if(min <= max){	
+		if(array[mid] < value){
+			binarySearchRecursive(array, value, min, mid - 1);
+		} else if (array[mid] > value){
+			binarySearchRecursive(array, value, mid + 1, max);
 		} else {
 			return mid;
 		}
