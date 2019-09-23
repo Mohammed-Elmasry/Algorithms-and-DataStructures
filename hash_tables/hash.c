4f8c5d40a83b5ad3a7d08c57442a8a6b66457b13
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-23 13:19:24 +0200

    define printArray() to display an array's elements

--- hash_tables/hash.c
+++ hash_tables/hash.c
@@ -5,14 +5,37 @@
 //prototypes
 
 int main(int argc, char * argv[]){
-	int result = hash(13513254, 7);
-	printf("%d\n", result);
+	int arr[10] = {};
+	printArray(arr, 10);
 	return 0;
 }
-
+/**
+ * [hash a value to map it to a bucket in hash table]
+ * @param  input [value to hash]
+ * @param  len   [length of hash into]
+ * @return       [hash key]
+ */
 int hash(unsigned int input, int len){
 	int retval = 0;
 	retval = input % len;
 	return retval;
-	
+}
+
+/**
+ * [insertPair inserts key values into hash table]
+ * @param  array [array to insert into]
+ * @param  value [value to insert]
+ * @param  size  [size to hash unto]
+ * @return       [integer denoting result of insertion process]
+ */
+// int insertPair(int * array, int value, int size){
+// 	int key = hash(value, size);
+// 	if(array[key])
+// }
+
+void printArray(int * array, int size){
+	for (int i = 0; i < size; ++i){
+		printf("%d\n", array[i]);
+	}
+	puts("display complete..");
 }
--- hash_tables/hash.h
+++ hash_tables/hash.h
@@ -1,1 +1,2 @@
 int hash(unsigned int input, int len);
+void printArray(int * array, int size);
